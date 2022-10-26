5f1fe6927de2f7b61e0bca3b984faaa5320f24b7
Joy <mmehedihasanjoyv@gmail.com>
2022-10-26 14:49:40 +0600

    added my client site for testing purpose  without data

--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AuthProvider, { AuthContext } from './context/AuthProvider/AuthProvider';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>    
     <App />
- </AuthProvider>
-
   </React.StrictMode>
 );
 
